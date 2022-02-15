acd16f7dc6572a98122132d2d401b4ade50e6c6a
Bob Belderbos <bobbelderbos@gmail.com>
2022-02-15 12:55:22 +0100

    actually need to put this in tear down, not setup!

--- test_script.py
+++ test_script.py
@@ -7,11 +7,12 @@ import script
 
 
 @pytest.fixture(autouse=True)
-def delete_previous_run():
-    for file in os.listdir("."):
-        if re.match(r"sample-\d+\.csv", file):
-            print("remove file", file)
-            os.remove(file)
+def delete_previous_run(request):
+    def remove_csv_output_files():
+        for file in os.listdir("."):
+            if re.match(r"sample-\d+\.csv", file):
+                os.remove(file)
+    request.addfinalizer(remove_csv_output_files)
 
 
 def test_chunk_csv():
