1f724205030ed4339b9fa3aa70341a8fe0a4165f
Bob Belderbos <bobbelderbos@gmail.com>
2022-02-15 13:20:07 +0100

    get rid of hardcoded sample, base output name on input file name

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import csv
+from pathlib import Path
 import time
 import sys
 
@@ -13,12 +14,13 @@ def chunk_csv(input_file):
     outfile_number = 1
     outfile = None
 
+    input_file_base = Path(input_file).stem
     with open(input_file, 'r') as infile:
         for index, row in enumerate(csv.reader(infile)):
             if index % chunk_size == 0:
                 if outfile is not None:
                     outfile.close()
-                outfile_name = 'sample-{}.csv'.format(outfile_number)
+                outfile_name = f"{input_file_base}-{outfile_number}.csv"
                 outfile = open(outfile_name, 'w')
                 outfile_number += 1
                 writer = csv.writer(outfile)
