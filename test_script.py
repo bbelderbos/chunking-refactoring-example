408c796d95331f95f29d93d90a41dc7b6687af52
Bob Belderbos <bobbelderbos@gmail.com>
2022-02-15 13:10:09 +0100

    add more testing

--- test_script.py
+++ test_script.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import os
 import re
 
@@ -6,6 +7,7 @@ import pytest
 import script
 
 CSV_OUTPUT_FILE_PATTERN = re.compile(r"sample-\d+\.csv")
+Content = namedtuple("Content", "num_lines first_line last_line")
 
 
 @pytest.fixture(autouse=True)
@@ -18,8 +20,38 @@ def delete_previous_run(request):
 
 
 def test_chunk_csv():
+    # number of output files
     with open("sample.csv") as f:
         num_chunks = len(f.readlines()) // script.chunk_size + 1
     output_files = [file for file in os.listdir(".")
                     if CSV_OUTPUT_FILE_PATTERN.match(file)]
     assert len(output_files) == num_chunks
+
+    # length and offsets of content
+    actual_line_lengths = {}
+    for o in sorted(output_files):
+        with open(o) as foo:
+            lines = foo.readlines()
+            content = Content(
+                len(lines),
+                lines[0].strip(),
+                lines[-1].strip())
+            actual_line_lengths[o] = content
+
+    expected_content = {
+        "sample-1.csv":
+        Content(10000, "First Name,Last Name", "John ,Doe 9999"),
+        "sample-2.csv":
+        Content(10000, "John ,Doe 10000", "John ,Doe 19999"),
+        "sample-3.csv":
+        Content(10000, "John ,Doe 20000", "John ,Doe 29999"),
+        "sample-4.csv":
+        Content(10000, "John ,Doe 30000", "John ,Doe 39999"),
+        "sample-5.csv":
+        Content(10000, "John ,Doe 40000", "John ,Doe 49999"),
+        "sample-6.csv":
+        Content(10000, "John ,Doe 50000", "John ,Doe 59999"),
+        "sample-7.csv":
+        Content(1924, "John ,Doe 60000", "John ,Doe 61923"),
+    }
+    assert actual_line_lengths == expected_content
