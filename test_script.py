0ea61199967ea50ea2f99707d9d846690ec7a0d3
Bob Belderbos <bobbelderbos@gmail.com>
2022-02-15 12:58:39 +0100

    first test

--- test_script.py
+++ test_script.py
@@ -5,15 +5,21 @@ import pytest
 
 import script
 
+CSV_OUTPUT_FILE_PATTERN = re.compile(r"sample-\d+\.csv")
+
 
 @pytest.fixture(autouse=True)
 def delete_previous_run(request):
     def remove_csv_output_files():
         for file in os.listdir("."):
-            if re.match(r"sample-\d+\.csv", file):
+            if CSV_OUTPUT_FILE_PATTERN.match(file):
                 os.remove(file)
     request.addfinalizer(remove_csv_output_files)
 
 
 def test_chunk_csv():
-    pass
+    with open("sample.csv") as f:
+        num_chunks = len(f.readlines()) // script.chunk_size + 1
+    output_files = [file for file in os.listdir(".")
+                    if CSV_OUTPUT_FILE_PATTERN.match(file)]
+    assert len(output_files) == num_chunks
